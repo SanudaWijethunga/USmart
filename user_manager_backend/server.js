609bb20ddc68ee9fc24e0fc478d9a2d53bae8c59
Sanuda Wijethunga <165463798+SanudaWijethunga@users.noreply.github.com>
2025-03-31 00:07:28 +0530

    Setting up the backend for user sign up

--- user_manager_backend/server.js
+++ user_manager_backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/userRoutes');
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-connectDB();
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => console.log(
     'Server running on port 5000'
